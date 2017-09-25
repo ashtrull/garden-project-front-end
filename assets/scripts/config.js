ae5cd2d3aa5d50ac37354f4877b0dfff2c8b8a60
Ash Trull <ashtrull@gmail.com>
2017-09-24 21:41:51 -0400

    Just changing config.js

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -3,8 +3,8 @@
 const config = {
   apiOrigins: {
     // production: 'https://seedsower.herokuapp.com/',
-    // production: 'http://localhost:4741',
-    // development: 'https://seedsower.herokuapp.com/',
+    production: 'http://localhost:4741',
+    // development: 'https://seedsower.herokuapp.com/'
     development: 'http://localhost:4741'
   }
 }
