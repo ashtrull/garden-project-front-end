91fa7435405b1a078a2260cb7ee22c7f9cfb767a
Ash Trull <ashtrull@gmail.com>
2017-09-24 22:36:59 -0400

    Test PATCH & DELETE, worked on heroku

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -3,9 +3,9 @@
 const config = {
   apiOrigins: {
     // production: 'https://seedsower.herokuapp.com/',
-    production: 'http://localhost:4741',
-    // development: 'https://seedsower.herokuapp.com/'
-    development: 'http://localhost:4741'
+    // production: 'http://localhost:4741',
+    development: 'https://seedsower.herokuapp.com/'
+    // development: 'http://localhost:4741'
   }
 }
 
